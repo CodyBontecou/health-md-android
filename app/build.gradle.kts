e02301ae040b8eb333930a0cf1d836c77ccfeced
Cody Bontecou <bontecouc@gmail.com>
2026-07-27 07:14:17 -0400

    Bump Android release version code to 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.healthmd.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 21
+        versionCode = 22
         versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
